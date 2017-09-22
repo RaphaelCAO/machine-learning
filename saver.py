ec9bfc72ddcda933955c68f9f7f67667f73661ab
RaphaelCAO <31284648+RaphaelCAO@users.noreply.github.com>
2017-09-22 14:24:13 +0100

    Add files via upload
    save and restore variables such as weights and biases

---
+++ saver.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tensorflow as tf
+
+def restore(path):
+    Weight = tf.Variable([2,1,1],dtype = tf.float32)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        old_Weight = sess.run(Weight)
+        saver.restore(sess,path)
+        print "old Weight:",old_Weight,"\t--->\t","new Weight:",sess.run(Weight)
+
+def save(path):
+    Weight = tf.Variable([11,22,33],dtype = tf.float32)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.save(sess,path)
+        print sess.run(Weight)
+
+# save("data_lib/save.ckpt")
+restore("data_lib/save.ckpt")
