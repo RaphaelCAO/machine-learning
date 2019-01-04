1dc7397b78516071bca9fde2c8a91829d19e10d5
RaphaelCAO <31284648+RaphaelCAO@users.noreply.github.com>
2019-01-04 08:43:43 +0800

    Update pb_inference.py

--- pb_inference.py
+++ pb_inference.py
@@ -2,7 +2,6 @@ import re
 import numpy as np
 import os
 import tensorflow as tf
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import cv2
 from object_detection.utils import ops as utils_ops
@@ -15,7 +14,7 @@ class Model(object):
         # load the labels
         with open(PATH_TO_LABELS) as f:
             text = str(f.readlines())
-            pattern = r"name:\s?\"(\w*)\""
+            pattern = r"name:\s?\'(\w*)\'"
             self.label_names = re.findall(pattern, text)
 
         # initialize 10 common colors
@@ -35,34 +34,40 @@ class Model(object):
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
-    def run_inference_for_single_image(self, image_path):
-        """
-        inference one image
-        :param image_path: image path
-        :return: the prediction result
-        """
-        image_np = cv2.imread(image_path)
-
-        # with self.detection_graph.as_default():
-        with tf.Session(graph=self.detection_graph) as sess:
+            self.sess = tf.Session(graph=self.detection_graph)
             # Get handles to input and output tensors
             ops = tf.get_default_graph().get_operations()
             all_tensor_names = {output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
+            self.tensor_dict = {}
             for key in [
                 'num_detections', 'detection_boxes', 'detection_scores',
                 'detection_classes', 'detection_masks'
             ]:
                 tensor_name = key + ':0'
                 if tensor_name in all_tensor_names:
-                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                    self.tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
                         tensor_name)
-            if 'detection_masks' in tensor_dict:
+
+            self.image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+    def run_inference_for_single_image(self, image):
+        """
+        inference one image
+        :param image: image path
+        :return: the prediction result
+        """
+        if type(image) is str:
+            image_np = cv2.imread(image)
+        else:
+            image_np = image
+
+        with self.detection_graph.as_default():
+            if 'detection_masks' in self.tensor_dict:
                 # The following processing is only for single image
-                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                detection_boxes = tf.squeeze(self.tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(self.tensor_dict['detection_masks'], [0])
                 # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                real_num_detection = tf.cast(self.tensor_dict['num_detections'][0], tf.int32)
                 detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
                 detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
                 detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
@@ -70,12 +75,11 @@ class Model(object):
                 detection_masks_reframed = tf.cast(
                     tf.greater(detection_masks_reframed, 0.5), tf.uint8)
                 # Follow the convention by adding back the batch dimension
-                tensor_dict['detection_masks'] = tf.expand_dims(
+                self.tensor_dict['detection_masks'] = tf.expand_dims(
                     detection_masks_reframed, 0)
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
             # Run inference
-            output_dict = sess.run(tensor_dict,
-                                   feed_dict={image_tensor: np.expand_dims(image_np, 0)})
+            output_dict = self.sess.run(self.tensor_dict, feed_dict={self.image_tensor: np.expand_dims(image_np, 0)})
             # all outputs are float32 numpy arrays, so convert types as appropriate
             output_dict['num_detections'] = int(output_dict['num_detections'][0])
             output_dict['detection_classes'] = output_dict[
@@ -86,15 +90,15 @@ class Model(object):
                 output_dict['detection_masks'] = output_dict['detection_masks'][0]
         return output_dict
 
-    def visualize_prediction(self, img_path, score_thres=0.3, window_name='window'):
+    def visualize_prediction(self, image, score_thres=0.3, window_name='window'):
         """
         Given a picture path, visualize the bounding boxes of predicted objects.
-        :param img_path: the path of an image
+        :param img: the path of an image
         :param score_thres: the score threshold for visible bounding box
         :return: None
         """
-        output_dict = self.run_inference_for_single_image(img_path)
-        image_np = cv2.imread(img_path)
+        output_dict = self.run_inference_for_single_image(image)
+        image_np = cv2.imread(image) if type(image) is str else image
 
         scores = output_dict['detection_scores']
         boxes = output_dict['detection_boxes']
@@ -108,11 +112,7 @@ class Model(object):
             pt1 = (int(box[1] * width), int(box[0] * height))
             pt2 = (int(box[3] * width), int(box[2] * height))
             color = self.colors[label-1]
-            print(color)
-            try:
-                cv2.rectangle(image_np, pt1, pt2, color, 2)
-            except:
-                print(color)
+            cv2.rectangle(image_np, pt1, pt2, color, 2)
 
             title = "{}: {}".format(self.label_names[label-1], score)
             bar_width = np.ceil(score * (pt2[0]-pt1[0])).astype(np.int)
